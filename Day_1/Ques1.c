e5e2dec27aadf1be3ed8bfad3d2594974a556157
Praroop Maurya <mauryapraroop7@gmail.com>
2026-05-27 23:03:09 +0530

    day 1 completed

---
+++ Day_1/Ques1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter the number N you want sum of:");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    printf("Sum of first %d natural numbers is %d", n, sum);
+
+    return 0;
+}
