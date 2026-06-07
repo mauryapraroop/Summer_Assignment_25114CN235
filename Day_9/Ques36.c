14217a0d41e8efa8160a240ef323cf7ed1fb5c46
Praroop Maurya <mauryapraroop7@gmail.com>
2026-06-07 13:19:40 +0530

    Day 9 Completed

---
+++ Day_9/Ques36.c
@@ -0,0 +1,34 @@
+/*
+
+PATTERN TO BE PRINTED :
+
+*****
+*   *
+*   *
+*   *
+*****
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j, n ;
+
+
+     printf("enter the number of rows you want to print:");
+    scanf("%d", &n);
+
+    
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n; j++) {
+            if(i == 1 || i == n || j == 1 || j == n)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
